5509f96fc9dd2d7277994285bc2514dd360f2955
m01ibDEN <maxachev.danila@gmail.com>
2023-11-14 17:41:02 +0300

    added test comment

--- src/main/java/com/m01libDEN/ForAnInternship/ForAnInternshipApplication.java
+++ src/main/java/com/m01libDEN/ForAnInternship/ForAnInternshipApplication.java
@@ -3,6 +3,7 @@ package com.m01libDEN.ForAnInternship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// application for internship
 @SpringBootApplication
 public class ForAnInternshipApplication {
 
